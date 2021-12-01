151d2a1fe0ab0933ee94d204c6b6f413f665ced1
Yael <yaeliberman123@gmail.com>
2021-12-01 18:39:36 +0200

    change path dataset

--- final_project/datasetCreate.py
+++ final_project/datasetCreate.py
@@ -28,7 +28,8 @@ class dataset:
             perturbed_four_points.append(
                 (point[0] + random.randint(-rho, rho), point[1] + random.randint(-rho, rho)))
 
-        H = cv2.getPerspectiveTransform(np.float32(four_points), np.float32(perturbed_four_points))
+        H = cv2.getPerspectiveTransform(np.float32(
+            four_points), np.float32(perturbed_four_points))
         H_inverse = inv(H)
 
         warped_image = cv2.warpPerspective(img, H_inverse, (320, 240))
@@ -47,16 +48,22 @@ class dataset:
         # './temp_photos/warped_image.jpg'
 
         cv2.imwrite(resize_Path, cv2.cvtColor(img, cv2.COLOR_RGB2BGR))
-        cv2.imwrite(homography_path, cv2.cvtColor(warped_image, cv2.COLOR_RGB2BGR))
+        cv2.imwrite(homography_path, cv2.cvtColor(
+            warped_image, cv2.COLOR_RGB2BGR))
         return warped_image
 
-    def PicturesInFolder(self, folderPath):
-        resize_path = './photos'
-        homography_path = './homography_photos'
+    def PicturesInFolder(self, folderPath, resize_path, homography_path):
+        # resize_path = './photos'
+        # homography_path = './homography_photos'
         for file in os.scandir(folderPath):
+            print(file)
             self.ImagePreProcessing(folderPath + '/' + file.name, resize_path + '/' + file.name,
                                     homography_path + '/' + file.name)
 
 
 d = dataset()
-d.PicturesInFolder("./temp_photos")
+# d.PicturesInFolder("./temp_photos")
+path = '../datasets/paris_1/paris/eiffel'
+resize_path = '../resize_datasets/eiffel'
+homography_path = '../homography_datasets/eiffel'
+d.PicturesInFolder(path, resize_path, homography_path)
