7934ec72fad48fa461a17763216fad714fbc14af
Yael <yaeliberman123@gmail.com>
2021-12-21 20:41:29 +0200

    score M*, I*, J*

--- final_project/main.py
+++ final_project/main.py
@@ -6,6 +6,13 @@ from numpy.linalg import inv
 import matplotlib.pyplot as plt
 
 
+def ArrayToKeyPoints(arr):
+    kp = []
+    for k in arr:
+        kp.append(cv2.KeyPoint(k[0][0], k[0][1], k[1], k[2], k[3], k[4], k[5]))
+    return kp
+
+
 def get_best_matches(desc1, desc2):
     print("--------- In get_best_matches ---------")
     FLANN_INDEX_KDTREE = 1
@@ -91,13 +98,9 @@ def make_match(path1, path2, path3):
     plt.imshow(img2)
     plt.show()
 
-    kp1 = []
-    kp2 = []
     # extract keyPoints from params we made on dataSetCreate
-    for k in data['kp1']:
-        kp1.append(cv2.KeyPoint(k[0][0], k[0][1], k[1], k[2], k[3], k[4], k[5]))
-    for k in data['kp2']:
-        kp2.append(cv2.KeyPoint(k[0][0], k[0][1], k[1], k[2], k[3], k[4], k[5]))
+    kp1 = ArrayToKeyPoints(data['kp1'])
+    kp2 = ArrayToKeyPoints(data['kp2'])
 
     desc1, desc2 = data['desc1'], data['desc2']
 
@@ -105,25 +108,100 @@ def make_match(path1, path2, path3):
 
     H, mask, img2_warped = find_homography(img1, img2, kp1, kp2, best_matches)
 
+    match_score = get_match_score(kp1, kp2, best_matches, data['M'], data['I'], data['J'])
+
     print_wraped_images(img1, img2, img2_warped)
 
     return H
 
 
+def get_match_score(kp1, kp2, best_matches, M, I, J):
+    print("--------- In get_match_score ---------")
+    # extract keyPoints from params we made on dataSetCreate
+    m_source = ArrayToKeyPoints(M[0])
+    m_dest = ArrayToKeyPoints(M[1])
+
+    M = [m_source, m_dest]
+
+    src_pts = np.float32([kp1[m.queryIdx].pt for m in best_matches])
+    dst_pts = np.float32([kp2[m.trainIdx].pt for m in best_matches])
+
+    M_ = [src_pts, dst_pts]
+    I_ = [item for item in kp1 if item.pt not in src_pts]
+    J_ = [item for item in kp2 if item.pt not in dst_pts]
+
+    M_counter = 0
+    print("len M  ", len(M[0]))
+    print("len M* ", len(M_[0]))
+    for j in range(len(M_[0])):
+        for i in range(len(M[0])):
+            if M[0][i].pt[0] == M_[0][j][0] and M[0][i].pt[1] == M_[0][j][1] \
+                    and M[1][i].pt[0] == M_[1][j][0] and M[1][i].pt[1] == M_[1][j][1]:
+                M_counter += 1
+                break
+    I = ArrayToKeyPoints(I)
+    print("len I  ", len(I))
+    print("len I* ", len(I_))
+    I_counter = 0
+    for kp_1 in I_:
+        for kp_2 in I:
+            if kp_1.pt[0] == kp_2.pt[0] and kp_1.pt[1] == kp_2.pt[1]:
+                I_counter += 1
+                break
+
+    J = ArrayToKeyPoints(J)
+    print("len J  ", len(J))
+    print("len J* ", len(J_))
+    J_counter = 0
+    for kp_1 in J_:
+        for kp_2 in J:
+            if kp_1.pt[0] == kp_2.pt[0] and kp_1.pt[1] == kp_2.pt[1]:
+                J_counter += 1
+                break
+
+    print("-----------------")
+    print("M_counter: ", M_counter)
+    print("I_counter: ", I_counter)
+    print("J_counter: ", J_counter)
+    score = (M_counter + I_counter + J_counter) / (len(M[0]) + len(I) + len(J))
+    print("match score: ", score)
+
+    return score
+
+
 def H_error(H_dest_to_src, path):
     # the func return the distance of H.dot(H*) from I
     data = np.load(path, allow_pickle=True)
-    H_src_to_dest = data['H'] # Homograpy matrix from src to dest
+    H_src_to_dest = data['H']  # Homograpy matrix from src to dest
     error = H_src_to_dest.dot(H_dest_to_src) - np.eye(3)
     error = np.sum(np.abs(error))
-    return error
+
+    H_mean, H_std = getDifficultLevel(H_src_to_dest)
+
+    return error, H_mean, H_std
+
+
+def getDifficultLevel(H):
+    I = np.eye(3)
+    dif = np.abs(H - I)
+    H_mean = np.mean(dif)
+    H_std = np.std(dif)
+    return H_mean, H_std
 
 
 if __name__ == '__main__':
-    file_name = "room2.jpeg"
-    path1 = "./photos/" + file_name
-    path2 = "./homography_photos/" + file_name
-    path3 = "./params/" + file_name + ".npz"
+    file_name = "Eiffel.jpg"
+    path1 = "./data/resize_photos/" + file_name
+    path2 = "./data/homography_photos/2/" + file_name
+    path3 = "./data/params/2/" + file_name + ".npz"
     H_dest_to_src = make_match(path1, path2, path3)
-    error = H_error(H_dest_to_src, path3)
+    error, H_mean, H_std = H_error(H_dest_to_src, path3)
     print("error: ", error)
+    print("H_mean: ", H_mean)
+    print("H_std: ", H_std)
+
+    # path2 = "./data/homography_photos/2/" + file_name
+    # path3 = "./data/params/2/" + file_name + ".npz"
+    # H_dest_to_src = make_match(path1, path2, path3)
+    # error = H_error(H_dest_to_src, path3)
+    # print("error: ", error)
