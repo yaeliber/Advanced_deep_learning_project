06101819e3b61286e73b328f0dcaee70fdf22059
Yaniv-Gilad <Yanivgilad96@gmail.com>
2022-04-28 21:31:52 +0300

    added comments

--- final_project/tensorUtils.py
+++ final_project/tensorUtils.py
@@ -3,6 +3,8 @@ import cv2
 import torch
 
 
+# param: list of keypoints
+# return: tensor of keypoints
 def array_to_tensor_of_key_points(arr):
     kp = []
     for k in arr:
@@ -10,22 +12,29 @@ def array_to_tensor_of_key_points(arr):
     return torch.stack((kp))
 
 
+# param: kp1, kp2 are list of keypoints
+#        matches are (Dmatch) list of matches between kp1 and kp2
+# return: 2 tensors of source and destination from matches
 def Dmatch_to_src_dst_tensors(kp1, kp2, matches):
     src = []
     dst = []
-    print("line 16 matches",  matches)
+    print("line 16 matches", matches)
     for m in matches:
         src.append(torch.Tensor([kp1[m.queryIdx][0], kp1[m.queryIdx][1]]))
         dst.append(torch.Tensor([kp2[m.trainIdx][0], kp2[m.trainIdx][1]]))
     return torch.stack((src)), torch.stack((dst))
 
-def kp_not_in_tensor(kp, O_tensor):
+
+# param: kp is list of keypoints
+#        tensor_kp is a tensor of keypints (only x and y coordinates)
+# return: tensor of keypoints that doesnt exist in tensor_kp
+def kp_not_in_tensor(kp, tensor_kp):
     output = []
     for item in kp:
         found = False
         x = item[0]
         y = item[1]
-        for point in O_tensor:
+        for point in tensor_kp:
             if point[0] == x and point[1] == y:
                 found = True
                 break
@@ -33,6 +42,8 @@ def kp_not_in_tensor(kp, O_tensor):
             output.append(item)
     return torch.stack((output))
 
+
+# return: score of matching (according to M, I, J score)
 def get_match_score_tensor(kp1, kp2, best_matches, M, I, J):
     print('--------- In get_match_score ---------')
     # extract keyPoints from params we made on dataSetCreate
@@ -41,15 +52,17 @@ def get_match_score_tensor(kp1, kp2, best_matches, M, I, J):
         print("no matches")
         return 0
 
+    # 2 tensors of source and destination from matches
     src_pts, dst_pts = Dmatch_to_src_dst_tensors(kp1, kp2, best_matches)
 
     M_ = torch.stack(([src_pts, dst_pts]))
     I_ = kp_not_in_tensor(kp1, src_pts)
     J_ = kp_not_in_tensor(kp2, dst_pts)
 
-    M_counter = 0
     print('len M  ', len(M[0]))
     print('len M* ', len(M_[0]))
+    M_counter = 0
+    # M_counter is counting intersection between M and M_ (M_ = M*)
     for j in range(len(M_[0])):
         for i in range(len(M[0])):
             if M[0][i][0] == M_[0][j][0] and M[0][i][1] == M_[0][j][1] \
@@ -60,6 +73,7 @@ def get_match_score_tensor(kp1, kp2, best_matches, M, I, J):
     print('len I  ', len(I))
     print('len I* ', len(I_))
     I_counter = 0
+    # I_counter is counting intersection between I and I_ (I_ = I*)
     for kp_1 in I_:
         for kp_2 in I:
             if kp_1[0] == kp_2[0] and kp_1[1] == kp_2[1]:
@@ -69,6 +83,7 @@ def get_match_score_tensor(kp1, kp2, best_matches, M, I, J):
     print('len J  ', len(J))
     print('len J* ', len(J_))
     J_counter = 0
+    # J_counter is counting intersection between J and J_ (J_ = J*)
     for kp_1 in J_:
         for kp_2 in J:
             if kp_1[0] == kp_2[0] and kp_1[1] == kp_2[1]:
